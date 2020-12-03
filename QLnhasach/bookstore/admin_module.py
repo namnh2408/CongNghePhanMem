6ed0c85b7f1495ac10f99f3822d99190ca912d59
NAMNGUYEN\namng <itnguyennam2408@gmail.com>
2020-12-04 00:28:08 +0700

    class sach còn lỗi type int >< string

--- QLnhasach/bookstore/admin_module.py
+++ QLnhasach/bookstore/admin_module.py
@@ -2,6 +2,17 @@ from bookstore import admin, db
 from bookstore.models import TacGia, TheLoai, KeSach, Sach
 from flask_admin.contrib.sqla import ModelView
 
+class SachModelView(ModelView):
+    column_display_pk = False# hien thi
+    can_create = True
+    can_edit = True
+    can_delete = True
+    can_export = True
+    form_columns = ('masach', 'tensach', 'theloai', 'tacgia', 'kesach', 'dongia', 'soluongton',)
+
+
+
 admin.add_view(ModelView(TacGia, db.session))
 admin.add_view(ModelView(TheLoai, db.session))
-admin.add_view(ModelView(Sach, db.session))
+admin.add_view(ModelView(KeSach, db.session))
+admin.add_view(SachModelView(Sach, db.session))
--- QLnhasach/bookstore/models.py
+++ QLnhasach/bookstore/models.py
@@ -1,7 +1,8 @@
 from sqlalchemy import Column, Integer, Float, String, Date, ForeignKey
+from sqlalchemy import Table
 from sqlalchemy.orm import relationship
 from bookstore import db
-from flask_sqlalchemy import SQLAlchemy
+
 
 
 
@@ -13,7 +14,8 @@ class TacGia(db.Model):
     ngaysinh = Column(Date, nullable=True)
     ngaymat = Column(Date , nullable=True)
     quequan = Column(String(100), nullable=True)
-    #sachs = relationship('sach', backref='tacgia', lazy=False)
+    tacgiasach = relationship('Sach', backref='TacGia', lazy=True)
+    # tacgia_theloai = relationship('TheLoai', backref='TacGia', lazy=True)
 
     def __str__(self):
         return self.tentacgia
@@ -24,6 +26,10 @@ class KeSach(db.Model):
     make = Column(Integer, primary_key=True, autoincrement=True)
     chatlieu = Column(String(50), nullable= True)
     succhua = Column(Integer, nullable= True)
+    kesachs = relationship('Sach', backref='KeSach', lazy=True)
+
+    def __str__(self):
+        return self.make
 
 class TheLoai(db.Model):
     __tablename__ = 'theloai'
@@ -31,7 +37,8 @@ class TheLoai(db.Model):
     matheloai = Column(Integer, primary_key=True, autoincrement=True)
     tentheloai = Column(String(100), nullable=False)
     ngaythem = Column(Date, nullable= True)
-    #sachs = relationship('sach', backref='theloai', lazy=False)
+    theloaisach = relationship('Sach', backref='TheLoai', lazy=False)
+
     def __str__(self):
         return self.tentheloai
 
@@ -45,10 +52,17 @@ class Sach(db.Model):
     kesach = Column(Integer, ForeignKey(KeSach.make))
     dongia = Column(Float, default=0)
     soluongton = Column(Integer, default=0)
-    #image = Column(String(255), nullable= True)
+    image = Column(String(255), nullable=True)
 
     def __str__(self):
         return self.tensach
+
+# class HoaDon(db.Model):
+#     __tablename__ = 'hoadon'
+#
+#     mahoadon= Column(String(20), primary_key=True, nullable=False)
+#     tenphieunhap= Column(String(50), nullable=True)
+#     ngaynhap= Column(Date, nullable=True)
 
 
 if __name__ == '__main__':
